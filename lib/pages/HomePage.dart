e170d920f44006b563207cd782b5b2a1681015c2
Geetika37 <geetikadinesh3@gmail.com>
2024-03-09 16:08:41 +0530

    created

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sunstarmovers/pages/Row1.dart';
+import 'package:sunstarmovers/pages/Row2.dart';
 import 'package:sunstarmovers/pages/chart.dart';
 import 'package:sunstarmovers/pages/container2.dart';
 import 'package:sunstarmovers/pages/navBar.dart';
@@ -35,39 +37,224 @@ class _HomePageState extends State<HomePage> {
       ),
       drawer: navBar(),
 
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text("Overview",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18,fontFamily: 'Poppins'),),
-                SizedBox(width: 120,),
-                Text("Show:",style: TextStyle(fontSize: 14,color: Colors.grey,fontFamily: 'Poppins',fontWeight: FontWeight.w400),),
-                Text("This Year",style: TextStyle(fontSize: 14,fontFamily: 'Poppins',fontWeight: FontWeight.w400),),
-                Image(image: AssetImage("assets/ic_Dropdown.png"))
-              ],
-            ),
-            SizedBox(height: 20,),
-            Row(
-              children: [
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
 
-                Flexible(child: HomeCard(title: 'Total Surveys',title2: '+2.5%',title3: '456',image: 'assets/Group 28.png',)),
-                SizedBox(width: 15,),
-                Flexible(child: HomeCard(title: 'Total activities',title2: '+2.5%',title3: '378',image: 'assets/Group 427318358.png',),)
-              ],
-            ),
-            Container(
+              Row(
+                children: [
+                  Text("Overview",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18,fontFamily: 'Poppins'),),
+                  SizedBox(width: 120,),
+                  Text("Show:",style: TextStyle(fontSize: 14,color: Colors.grey,fontFamily: 'Poppins',fontWeight: FontWeight.w400),),
+                  Text("This Year",style: TextStyle(fontSize: 14,fontFamily: 'Poppins',fontWeight: FontWeight.w400),),
+                  Image(image: AssetImage("assets/ic_Dropdown.png"))
+                ],
+              ),
+              SizedBox(height: 20,),
+
+              Row(
+                children: [
+        
+                  Flexible(child: HomeCard(title: 'Total Surveys',title2: '+2.5%',title3: '456',image: 'assets/Group 28.png',)),
+                  SizedBox(width: 15,),
+                  Flexible(child: HomeCard(title: 'Total activities',title2: '+2.5%',title3: '378',image: 'assets/Group 427318358.png',),)
+                ],
+              ),
+              SizedBox(height: 15,),
+        
+              Container(
+                padding: EdgeInsets.all(10),
+                height: 360,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3)
+                    )
+                  ]
+                ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 5,),
+                      Row(
+                        children: [
+                          SizedBox(width: 10,),
+                          Text("Survey Report",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,fontFamily: 'Poppins',),),
+                          SizedBox(width: 110,),
+                          Text("2017-2018",style: TextStyle(fontSize: 13,fontWeight: FontWeight.w400,fontFamily: 'Poppins',color: Colors.grey),),
+        
+                        ],
+                      ),
+                      SizedBox(height: 15,),
+                      Chart(),
+        
+                      Divider(
+                        height: 10,
+                        color: Colors.grey.shade300,
+                        thickness: 1,
+                        indent : 10,
+                        endIndent : 10,
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          SizedBox(width: 50,),
+                            Row1(image: 'assets/Oval.png',title1: ' Last year',title2: '989',),
+                            SizedBox(width: 40,),
+                            Row1(image: 'assets/Oval_pic.png',title1: ' This Year ',image1: 'assets/up.png',title2: '1223',title3: " 1.2%",)
+                        ],
+                      )
+                    ],
+                  )
+              ),
+
+              SizedBox(height: 20,),
+        
+              Container(
+                // padding: EdgeInsets.all(10),
+                height: 310,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3)
+                    )
+                  ]
+                ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Chart()
-                  ],
-                )
-            ),
+                    SizedBox(height: 10,),
+                    
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 10,),
+                          Text("Surveys",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18,fontFamily: 'Poppins'),),
+                          SizedBox(width: 150,),
+                          Text("View all",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 13,fontFamily: 'Poppins',color: Colors.red),)
+                        ],
+                      ),
+                    ),
 
-          ],
+                    new Divider(
+                      color: Colors.grey.shade300,
+                    ),
+                    SizedBox(height: 10,),
+
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(width: 20,),
+                            Row2(name: "James Wan", image1: "assets/Group 162507.png", date: "12/12/2023", time: "12:45 PM", image2: 'assets/calendar.png', image3: 'assets/clock.png', percentage: '75%',),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+
+                        new Divider(
+                          color: Colors.grey.shade300,
+                        ),
+                        SizedBox(height: 10,),
+
+
+                        Row(
+                          children: [
+                            SizedBox(width: 20,),
+                            Row2(name: "Peter Parker", image1: "assets/Group 162508.png", date: "12/12/2023", time: "12:45 PM", image2: 'assets/calendar.png', image3: 'assets/clock.png', percentage: '10%',),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+
+                        new Divider(
+                          color: Colors.grey.shade300,
+                        ),
+                        SizedBox(height: 10,),
+
+                        Row(
+                          children: [
+                            SizedBox(width: 20,),
+                            Row2(name: "Zack Snyder", image1: "assets/Group 162509.png", date: "12/12/2023", time: "12:45 PM", image2: 'assets/calendar.png', image3: 'assets/clock.png', percentage: '100%',),
+                          ],
+                        ),
+
+
+                      ],
+                    )
+
+
+                  ],
+                ),
+              ),
+
+            ],
+          ),
         ),
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items:  [
+
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/home.png"),
+              size: 20,
+              color: Colors.red,
+            ),
+            label: 'Home',
+          ),
+
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/file-02.png"),
+              size: 20,
+            ),
+            label: 'Survey',
+          ),
+
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage("assets/Icon.png"),
+              size: 24,
+              ),
+            label: 'Notification'
+          ),
+
+
+
+        ],
+        selectedItemColor: Colors.red,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 12),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 12),
+
+        onTap: (int index)
+        {
+          switch (index)
+          {
+            case 0:
+              Navigator.pop(context);
+              break;
+            case 1:
+              //Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+          }
+        }
+        ,
+      ),
+
+
+
+
     );
   }
 }
---
+++ lib/pages/Row2.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// import 'package:percent_indicator/percent_indicator.dart';
+
+class Row2 extends StatelessWidget {
+  final String name;
+  final String image1;
+  final String date;
+  final String time;
+  final String image2;
+  final String image3;
+  final String percentage;
+
+
+  const Row2({super.key, required this.name, required this.image1, required this.date, required this.time, required this.image2, required this.image3, required this.percentage, });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(name,style: TextStyle(fontWeight: FontWeight.w600,fontSize: 16,fontFamily: 'Poppins'),),
+                SizedBox(width: 15,),
+                Image(image: AssetImage(image1)),
+              ],
+            ),
+            SizedBox(height: 5,),
+            Row(
+              children: [
+                Image(image: AssetImage(image2)),
+                SizedBox(width: 5,),
+                Text(date,style: TextStyle(fontWeight: FontWeight.w400,fontSize: 12,fontFamily: 'Poppins',color: Colors.grey.shade500),),
+                SizedBox(width: 20,),
+                Image(image: AssetImage(image3)),
+                SizedBox(width: 5,),
+                Text(time,style: TextStyle(fontWeight: FontWeight.w400,fontSize: 12,fontFamily: 'Poppins',color: Colors.grey.shade500),)
+              ],
+            ),
+
+          ],
+        ),
+        SizedBox(width: 70,),
+
+        Column(
+          children: [
+            Stack(
+                alignment: Alignment.center,
+                children:[
+                  CircularProgressIndicator(
+                    color: Colors.yellow.shade800,
+                    backgroundColor: Colors.yellow.withOpacity(0.1),
+                    value: 0.5,
+                    strokeWidth: 6,
+                    strokeAlign: 2,
+                    semanticsLabel: '50%',
+                    strokeCap: StrokeCap.round,
+                  ),
+                  Text(percentage,style: TextStyle(color: Colors.yellow.shade800,fontWeight: FontWeight.bold),)
+                ]
+            )
+          ],
+        ),
+
+      ],
+    );
+  }
+}
